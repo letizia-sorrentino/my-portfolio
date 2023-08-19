8b20080b7178220df6df6b3f91c834d454d220d2
Letizia <116899981+letizia-sorrentino@users.noreply.github.com>
2023-08-19 16:21:16 +0200

    added styles

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,5 +1,5 @@
 import theSimpsonsApp from "../assets/theSimpsonsApp.png";
-
+import LoveFoodApp from "../assets/LoveFoodApp.png";
 import "../styles/Work.css";
 
 const Work = () => {
@@ -7,11 +7,29 @@ const Work = () => {
     <>
       <section className="intro">
         <h1 className="introduction">Frontend Engineer</h1>
-        <p className="techInfo"> JavaScript | React.js | Node.js</p>
+        <p className="techInfo"> React.js  | Redux.js | Node.js</p>
       </section>
       <section className="selectedWork">
         <h2>SELECTED WORK</h2>
         <div className="projectsCardsContainter">
+
+        <div className="projectCard">
+            <img
+              className="projectCardImage"
+              src={LoveFoodApp}
+              alt="Simpsons project"
+            />
+            <h3 className="projectTitle">Love Food App</h3>
+            <p className="tech">
+              REACT &#8226; REDUX TOOLKIT &#8226; CSS &#8226; NODE.JS &#8226;
+              EXPRESS.JS &#8226; SQL
+            </p>
+            <p className="projectCardDescription">
+              A full stack recipe app built with React Redux Toolkit, with data
+              provided by the Spoonacular API.
+            </p>
+          </div>
+
           <div className="projectCard">
             <img
               className="projectCardImage"
@@ -25,6 +43,8 @@ const Work = () => {
               provided by The Simpsons Quote API.
             </p>
           </div>
+
+          
         </div>
       </section>
     </>
